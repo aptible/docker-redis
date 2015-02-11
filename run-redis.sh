538815d7aa5093c780641c1e2b1a53b86c47ab68
jbergknoff <jonathan@bergknoff.com>
2015-02-10 18:17:05 -0600

    entrypoint script

---
+++ run-redis.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+CONF_FILE="/redis.conf"
+> "$CONF_FILE"
+echo "maxmemory ${REDIS_MAX_MEMORY:-100mb}" >> "$CONF_FILE"
+echo "maxmemory-policy ${REDIS_MAX_MEMORY_POLICY:-"allkeys-lru"}" >> "$CONF_FILE"
+
+if [[ -n "$REDIS_PASSWORD" ]]; then
+	echo "requirepass "$REDIS_PASSWORD"" >> "$CONF_FILE"
+fi
+
+redis-server "$CONF_FILE"
