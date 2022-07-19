98e4be634fa51de3c2808c9041b17f73f29bc967
Josh Raker <josh.raker@aptible.com>
2022-07-19 18:30:05 -0400

    Remove ACL channel patterns from Redis 6

--- bin/run-database.sh
+++ bin/run-database.sh
@@ -210,8 +210,14 @@ EOF
 create_user() {
   # Create an ACL file to store users in and create the initial user
   mkdir -p "$ACL_DIRECTORY"
-  echo "user ${USERNAME} on >${PASSPHRASE} ~* &* +@all" > "$ACL_FILE"
-  echo "user default on >${PASSPHRASE} ~* &* +@all" >> "$ACL_FILE"
+  if [[ -n "${VERSION_GTE_6_2:-}" ]]; then
+    echo "user ${USERNAME} on >${PASSPHRASE} ~* &* +@all" > "$ACL_FILE"
+    echo "user default on >${PASSPHRASE} ~* &* +@all" >> "$ACL_FILE"
+  else
+    # Redis 6 doesn't support pubsub channel patterns (&*)
+    echo "user ${USERNAME} on >${PASSPHRASE} ~* +@all" > "$ACL_FILE"
+    echo "user default on >${PASSPHRASE} ~* +@all" >> "$ACL_FILE"
+  fi
   echo "--aclfile ${ACL_FILE}" >> "$ARGUMENT_FILE"
   # The aclfile must be in a directory owned by the $REDIS_USER in order for
   # ACL SAVE to work as this command creates a temporary file in the same directory
